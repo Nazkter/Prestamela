bb563f16f15aba8b3c003944c34b1aea7a8a3d18
Miguel Gutierrez <ryuuzakiupldr@gmail.com>
2017-05-25 05:10:06 -1100

    cambios generales

--- prestapi/urls.py
+++ prestapi/urls.py
@@ -4,4 +4,5 @@ from prestapi import views
 
 urlpatterns = [
     url(r'^$', views.show_form, name='show_form'),
+    url(r'^$', views.ajax_get_score, name='ajax_get_score'),
     ]
--- prestapi/views.py
+++ prestapi/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from prestapi.models import Config, Client
 import json
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
 
 
 def show_privacy_policy(request):
@@ -37,14 +39,61 @@ def show_form(request):
     return JsonResponse( response )
 
 
-def get_score(request):
-    # 1 - Obtengo las credenciales y las cotejo con BD
-    # 2 - Tomo los datos entrantes y despliego la interfaz
+def ajax_get_score(request):
+    # First it gets the form data
+    user_email = request.POST.get('user_email', '')
+    user_password = request.POST.get('user_password', '')
+    document_type = request.POST.get('document_type', '')
+    document_id = request.POST.get('document_id', '')
+    first_name = request.POST.get('first_name', '')
+    second_name = request.POST.get('second_name', '')
+    last_name = request.POST.get('last_name', '')
+    second_last_name = request.POST.get('second_last_name', '')
+    expedition_date = request.POST.get('expedition_date', '')
+    birthdate = request.POST.get('birthdate', '')
+    gender = request.POST.get('gender', '')
+    email = request.POST.get('email', '')
+    phone = request.POST.get('phone', '')
+    civil_status = request.POST.get('civil_status', '')
+    address_residence = request.POST.get('address_residence', '')
+    type_of_property = request.POST.get('type_of_property', '')
+    work_activity = request.POST.get('work_activity', '')
+    work_type = request.POST.get('work_type', '')
+    personal_reference_first_name = request.POST.get('personal_reference_first_name', '')
+    personal_reference_second_name = request.POST.get('personal_reference_second_name', '')
+    personal_reference_last_name = request.POST.get('personal_reference_last_name', '')
+    personal_reference_second_last_name = request.POST.get('personal_reference_second_last_name', '')
+    personal_reference_phone = request.POST.get('personal_reference_phone', '')
 
-    return JsonResponse(
-        {
-            'success': True,
-            'error': False,
-            'msg': 'Petición correcta.',
-        }
-    );
+    # Then it set the required data for the credit score web service
+    url = 'http://dev.bio.credit/integration/prestame/give-a-score'
+    post_fields = {
+        'user_email': user_email,
+        'user_password': user_password,
+        'document_type': document_type,
+        'document_id': document_id,
+        'first_name': first_name,
+        'second_name': second_name,
+        'last_name': last_name,
+        'second_last_name': second_last_name,
+        'expedition_date': expedition_date,
+        'birthdate': birthdate,
+        'gender': gender,
+        'email': email,
+        'phone': phone,
+        'civil_status': civil_status,
+        'address_residence': address_residence,
+        'type_of_property': type_of_property,
+        'work_activity': work_activity,
+        'work_type': work_type,
+        'personal_reference_first_name': personal_reference_first_name,
+        'personal_reference_second_name': personal_reference_second_name,
+        'personal_reference_last_name': personal_reference_last_name,
+        'personal_reference_second_last_name': personal_reference_second_last_name,
+        'personal_reference_phone': personal_reference_phone,
+    }
+    # it makes the POST petition to the web service
+    request = Request(url, urlencode(post_fields).encode())
+    json = urlopen(request).read().decode()
+    print(json)
+    return JsonResponse(json);
